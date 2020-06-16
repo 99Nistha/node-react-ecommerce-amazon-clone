073faf1f77155f460105760f9573121ea7162dbb
99Nistha <nisthaagarwal8@gmail.com>
2020-06-16 16:04:49 +0530

    deatailing of products

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -32,27 +32,28 @@ function ProductScreen(props){
                     </li>
                 </ul>
             </div>
-            <div className="details-action"></div>
-            <ul>
-                <li>
-                    Price:<b>${product.price}</b>
-                </li>
-                <li>
-                    Status:{product.status}
-                </li>
-                <li>
-                    Qty:<select>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                    </select>
-                </li>
-                <li>
-                    <button>Add to cart</button>
-                </li>
-            </ul>
+            <div className="details-action">
+                <ul>
+                    <li>
+                        Price:<b>${product.price}</b>
+                    </li>
+                    <li>
+                        Status:{product.status}
+                    </li>
+                    <li>
+                        Qty:<select>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </li>
+                    <li>
+                        <button className="button">Add to cart</button>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 }
