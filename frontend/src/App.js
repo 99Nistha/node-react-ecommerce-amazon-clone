e74912d40895232e0b579a80168768663183913d
99Nistha <nisthaagarwal8@gmail.com>
2020-06-16 08:32:20 +0530

    making changes in app.js and css file of react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,111 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="grid-container">
+    <header className="header">
+      <div className="brand">
+        <button onclick="openMenu()">
+          &#9776;
+        </button>
+        <a href="index.html">amazona</a>
+      </div>
+      <div className="header-links">
+        <a href="cart.html">Cart</a>
+        <a href="signin.html">Sign In</a>
+      </div>
+    </header>
+    <aside className="sidebar">
+      <h3>Shopping Categories</h3>
+      <button className="sidebar-close-button" onclick="closeMenu()">x</button>
+      <ul>
+        <li>
+          <a href="index.html">Pants</a>
+        </li>
+
+        <li>
+          <a href="index.html">Shirts</a>
+        </li>
+
+      </ul>
+    </aside>
+    <main className="main">
+      <div className="content">
+        <ul className="products">
+          <li>
+            <div className="product">
+              <img className="product-image" src="images/d1.jpg" alt="product" />
+              <div className="product-name">
+                <a href="product.html">Slim Shirt</a>
+              </div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">$60</div>
+              <div className="product-rating">4.5 Stars (10 Reviews)</div>
+            </div>
+          </li>
+          <li>
+            <div className="product">
+              <img className="product-image" src="images/d1.jpg" alt="product" />
+              <div className="product-name">
+                <a href="product.html">Slim Shirt</a>
+              </div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">$60</div>
+              <div className="product-rating">4.5 Stars (10 Reviews)</div>
+            </div>
+          </li>
+          <li>
+            <div className="product">
+              <img className="product-image" src="images/d1.jpg" alt="product" />
+              <div className="product-name">
+                <a href="product.html">Slim Shirt</a>
+              </div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">$60</div>
+              <div className="product-rating">4.5 Stars (10 Reviews)</div>
+            </div>
+          </li>
+          <li>
+            <div className="product">
+              <img className="product-image" src="images/d1.jpg" alt="product" />
+              <div className="product-name">
+                <a href="product.html">Slim Shirt</a>
+              </div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">$60</div>
+              <div className="product-rating">4.5 Stars (10 Reviews)</div>
+            </div>
+          </li>
+          <li>
+            <div className="product">
+              <img className="product-image" src="images/d1.jpg" alt="product" />
+              <div className="product-name">
+                <a href="product.html">Slim Shirt</a>
+              </div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">$60</div>
+              <div className="product-rating">4.5 Stars (10 Reviews)</div>
+            </div>
+          </li>
+          <li>
+            <div className="product">
+              <img className="product-image" src="images/d1.jpg" alt="product" />
+              <div className="product-name">
+                <a href="product.html">Slim Shirt</a>
+              </div>
+              <div className="product-brand">Nike</div>
+              <div className="product-price">$60</div>
+              <div className="product-rating">4.5 Stars (10 Reviews)</div>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+
+    </main>
+    <footer className="footer">
+      All right reserved.
+    </footer>
+  </div>
   );
 }
 
