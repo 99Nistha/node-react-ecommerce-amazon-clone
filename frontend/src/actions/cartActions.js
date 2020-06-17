fa9ea36c6fae33a7e931e7dd02138b79c0676935
99Nistha <nisthaagarwal8@gmail.com>
2020-06-17 16:10:02 +0530

    advance react shooping cart screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,5 @@
 import Axios from "axios";
+import Cookie from "js-cookie";
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -14,11 +15,15 @@ const addToCart = (productId, qty) => async (dispatch, getState) => {
         qty
       }
     });
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
   } catch (error) {
 
   }
 }
 const removeFromCart = (productId) => (dispatch, getState) => {
     dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+    const { cart: { cartItems } } = getState();
+    Cookie.set("cartItems", JSON.stringify(cartItems));
 }
 export{addToCart, removeFromCart}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,12 @@
 import {createStore, combineReducers,applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
 import{productListReducer, productDetailsReducer} from './reducers/productReducers';
 import{cartReducer} from './reducers/cartReducers'
 
-const initialState={};
+const cartItems= Cookie.getJSON("cartItems") ||[];
+
+const initialState={cart:{cartItems}};
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
