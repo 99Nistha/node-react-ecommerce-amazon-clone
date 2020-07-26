95d899ea18209ca582e22a952a24323fa95da084
99Nistha <nisthaagarwal8@gmail.com>
2020-07-26 15:38:03 +0530

    uploads

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -3,7 +3,7 @@ import multer from 'multer';
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, '/uploads/');
+    cb(null, 'uploads/');
   },
   filename(req, file, cb) {
     cb(null, `${Date.now()}.jpg`);
