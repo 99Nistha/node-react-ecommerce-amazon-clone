7d51dda29c4c84a69077721fb2dc8f59e086f2e0
99Nistha <nisthaagarwal8@gmail.com>
2020-07-27 08:35:27 +0530

    uploads

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
     }
   } : {};
   const sortOrder = req.query.sortOrder ?
-    (req.query.sortOrder === 'lowest' ? { price: 1 } : { price: -1 })
+    (req.query.sortOrder === 'lowest' ? { price: -1 } : { price: +1 })
     :
     { _id: -1 };
   const products = await Product.find({ ...category, ...searchKeyword }).sort(sortOrder);
